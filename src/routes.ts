fe85ed88d5b1f5f8ea2a8ac3171283538d0e9ed6
vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-07-02 03:10:05 -0300

    Create pets with categorie

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -8,6 +8,7 @@ routes.get('/categories', async (request, response) => {
 
   const serializedCategories = categories.map(categorie => {
     return {
+      id: categorie.id,
       title: categorie.title,
       image_url: `http://localhost:3333/uploads/${categorie.image}`,
     };
@@ -15,5 +16,44 @@ routes.get('/categories', async (request, response) => {
 
   return response.json(serializedCategories);
 });
+
+routes.post('/pets', async (request, response) => {
+  const {
+    name,
+    email,
+    cellphone,
+    gender,
+    latitude,
+    longitude,
+    city,
+    uf,
+    additionalInfo,
+    categorie
+  } = request.body;
+
+  const ids = await knex('pets').insert({
+    image: 'image-fake',
+    name,
+    email,
+    cellphone,
+    gender,
+    latitude,
+    longitude,
+    city,
+    uf,
+    additionalInfo,
+  });
+
+  const petsCategorie = categorie.map((categorie_id: number) => {
+    return {
+      categorie_id,
+      pet_id: ids[0],
+    }
+  })
+
+  await knex('pets_categorie').insert(petsCategorie);
+
+  return response.json({message:"true"});
+})
 
 export default routes;
