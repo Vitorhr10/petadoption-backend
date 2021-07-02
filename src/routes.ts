1a7741270e8ebe4ff053bfea34f2afa1cd0f3696
vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-07-02 02:49:24 -0300

    Get categories with image URL

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+routes.get('/categories', async (request, response) => {
+  const categories = await knex('categories').select('*');
+
+  const serializedCategories = categories.map(categorie => {
+    return {
+      title: categorie.title,
+      image_url: `http://localhost:3333/uploads/${categorie.image}`,
+    };
+  })
+
+  return response.json(serializedCategories);
 });
 
 export default routes;
